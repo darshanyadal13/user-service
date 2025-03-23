e8aaf756cecb6f8907c4de114b2265408b5cdd29
darshanyadal <darshan.yadal@epifi.com>
2025-03-24 00:18:01 +0530

    cofigured security for spring security login

--- src/main/java/user_service/config/Security.java
+++ src/main/java/user_service/config/Security.java
@@ -1,13 +1,19 @@
 package user_service.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.beans.PropertyChangeListener;
@@ -15,6 +21,10 @@ import java.beans.PropertyChangeListener;
 @Configuration
 @EnableWebSecurity
 public class Security {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
@@ -27,11 +37,24 @@ public class Security {
 //        };
 //        http.csrf(custCsrf);
 
-                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/user/login").authenticated()
+                        .requestMatchers("/user/register").permitAll()
+                )
 //                .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
     }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
+        provider.setUserDetailsService(userDetailsService);
+        return provider;
+    }
+
+
 
 }
--- src/main/java/user_service/repository/UserRepository.java
+++ src/main/java/user_service/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package user_service.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import user_service.model.Users;
 
+@Repository
 public interface UserRepository extends JpaRepository<Users, Integer> {
+
+    Users findUserByUsername(String username);
 
 }
